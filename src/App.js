ac7ef8ef524ea458b37027c9492094229a17cdd7
dodycode <prasetyodody17@gmail.com>
2020-05-31 01:07:57 -0700

    Setup Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/Sidebar';
+import CreateAppointment from './pages/appointment/Create';
+import ListAppointment from './pages/appointment/List';
+import TodayAppointment from './pages/appointment/Today';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <CreateAppointment />
+      <ListAppointment />
+      <TodayAppointment />
+    </>
   );
 }
 
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component{
+	render(){
+		return (
+			<div>
+				<h3>Menu Disini</h3>
+			</div>
+		)
+	}
+}
+
+export default Sidebar;
